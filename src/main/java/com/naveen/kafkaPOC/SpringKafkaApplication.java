7bb63443068824316094349ca1e531dc3df9984a
Naveen <naveen.chaurasia861@gmail.com>
2022-07-19 17:37:22 +0530

    changed consumer and producer

--- src/main/java/com/naveen/kafkaPOC/SpringKafkaApplication.java
+++ src/main/java/com/naveen/kafkaPOC/SpringKafkaApplication.java
@@ -15,34 +15,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class SpringKafkaApplication implements ApplicationRunner {
-	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
-
-	public void sendMessage(String msg) {
-		System.out.println("msg:" + msg);
-		kafkaTemplate.send("naveen", msg);
-	}
+public class SpringKafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaApplication.class, args);
-	}
-
-	@KafkaListener(topics = "naveen", groupId = "group-id")
-	public void listen(String message) {
-		System.out.println(message);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		while (true) {
-			Thread.currentThread().sleep(1000);
-			sendMessage("{\"timestamp\":\"" + new Date() + "\", temp=" + randomNumber() + "}");
-		}
-	}
-
-	private double randomNumber() {
-		Random rand = new Random();
-		return rand.nextDouble();
 	}
 }
